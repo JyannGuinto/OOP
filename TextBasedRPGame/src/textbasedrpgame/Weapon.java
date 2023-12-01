63a51f60fefa5a466276af91bb78d3e9a654d5cf
JyannGuinto <152085989+JyannGuinto@users.noreply.github.com>
2023-12-01 14:23:10 +0800

    Add files via upload

---
+++ TextBasedRPGame/src/textbasedrpgame/Weapon.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package textbasedrpgame;
+
+/**
+ *
+ * @author Jyann 2
+ */
+public class Weapon {
+    String name;
+    int damage;
+    int price;
+}
